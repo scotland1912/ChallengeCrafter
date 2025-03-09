8c1ff973ee242b919bcedd81b5fd9b7c06cd9db5
scotland1912 <scotland1912@icloud.com>
2025-03-09 22:55:30 +0100

    Initial commit

---
+++ src/main/java/com/scotland1912/challengecrafter/ChallengeCrafterDataGenerator.java
@@ -0,0 +1,11 @@
+package com.scotland1912.challengecrafter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChallengeCrafterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
